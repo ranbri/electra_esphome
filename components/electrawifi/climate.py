b83e8c1bd38ea859e14a11b3e3f556cbd53dea88
Ran Bar Ilan <52109648+ranbri@users.noreply.github.com>
2026-04-13 09:47:08 +0000

    Update Electra ESPHome files

--- components/electrawifi/climate.py
+++ components/electrawifi/climate.py
@@ -1,9 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.const import CONF_ID
 from esphome import pins
 
 AUTO_LOAD = ["climate"]
